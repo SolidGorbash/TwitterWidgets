2a40491e91fff22e99b20ef632d2f364f60776db
SolidGorbash <jamesgorb@gmail.com>
2014-09-28 18:30:19 +0200

    Update define.php

--- example/php/define.php
+++ example/php/define.php
@@ -1,10 +1,10 @@
 <?php
 	
 // Definisco i dati di accesso per le API di Twitter
-define("CONSUMER_KEY", "t5nSPbteuEUgZxsw0aH4fjUcd");
-define("CONSUMER_SECRET", "nohwnUJX7Xcar0BxPpKsPXQlNoEPINRXFn0Sbtq1BZf38fBTzp");
-define("ACCESS_TOKEN", "321345207-jO0qQcbzBCEDYmWvdy7v4wb5N6jpcfUe9xxi8tzq");
-define("ACCESS_TOKEN_SECRET", "PjhL0qXej1LBxQhPHfTIouxyM23QGQDR2UHhsv32KKEqU");
+define("CONSUMER_KEY", "");
+define("CONSUMER_SECRET", "");
+define("ACCESS_TOKEN", "");
+define("ACCESS_TOKEN_SECRET", "");
 	
 	
 	
